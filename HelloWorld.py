3a4ada536fc81b551df2fb421500099c2ff15a43
sai9271 <sriramanujan123@gmail.com>
2023-10-03 11:18:45 +0530

    secondcommit

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,39 @@
 print("hello world")
+
+#Spelling Mistake in print
+prnt("Hello World!")
+
+Uppercase 'P' in print
+Print("hello World!")
+
+Missing quotes
+print(Hello world!)
+      
+Missing parentheses
+print("Hello world!"
+      
+      #Printing Without Quotes
+      #If we want to print the numercal result of 2+5, we do not add quotes
+
+      print(2+5)
+
+      #If we want to print the exact message "2+5", then we add the quotes
+      print ("2+5")
+
+      #calculations with python
+      #Addition is denoted by +
+      print(2+5)
+      print(1+1.5)
+      #subtraction is denoted by - sign
+      print(5-2)
+      
+      #Multiplication is denoted by * sign
+      print(2*5)
+      print(5*0.5)
+      
+      #Division is denoted by / sign
+      print(5/2)
+      print(4/2)
+
+      
+
