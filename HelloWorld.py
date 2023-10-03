4708aa06fdae4275fc1f66996e72b71f139c3a7b
sai9271 <sriramanujan123@gmail.com>
2023-10-03 15:06:24 +0530

    print three hashes

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,13 +3,13 @@ print("hello world")
 #Spelling Mistake in print
 prnt("Hello World!")
 
-#Uppercase 'P' in print
+Uppercase 'P' in print
 Print("hello World!")
 
-#Missing quotes
+Missing quotes
 print(Hello world!)
       
-#Missing parentheses
+Missing parentheses
 print("Hello world!"
       
       #Printing Without Quotes
@@ -35,62 +35,5 @@ print("Hello world!"
       print(5/2)
       print(4/2)
 
-      print("10+5")
-print(10+5)
-print("Rahul")
-print(25+5.1)
-print(CCBP)
-print("Age")
-print(10+5)
-print(4*9)
-print(1+1)
-print(1*3)
-print(12-12)
-print(1/1)
-print("A " + "@ " + "@ " + "Z")
-print("A" + " @" + " @ " + "Z")
-print("A " + "@" + " @" + " Z")
-print("A" + " @" * 2 + " Z")
-print("A " + "@ " * 2 + "Z")
-print("A " + "@ " + "@" + " Z")
-print("Stay" + " " + "Curious")
-print("Stay" + " Curious")
-print("Stay " + "Curious")
-print("StayCurious + " "")
-user = input()
-print("Hi " + user)
-user = input()
-print("Hi" + " " + user)
-user = input()
-print("Hi" + " user")
-word = "Think"
-word = word + ""
-
-print(word)
-word = "Think"
-word = "word "
-
-print(word)
-word = "Think"
-word = word + " "
-
-print(word)
-
-print("Think ")
-
-x = "12 * * 34"
-print(_____)
-1) x[3]	A) 1
-2) x[4]	B) Space (" ")
-3) x[7]	C) 3
-4) x[0]	D) *
-
-x = " " * 2 + "@" * 2
-print(_____)
-1) x[1]	A) Space (" ")
-2) len(x)	B) 4
-3) x[3]	C) @
-
-print(len("Create "))
-print(len("create" + " "))
+      
 
